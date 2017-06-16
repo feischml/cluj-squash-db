a27dcf8d1c669a4a39c2118b0db13a2ea822f9f9
Laszlo Feischmidt <feischml@Laszlos-MacBook-Pro.local>
2017-06-16 22:37:21 +0300

    second commit

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 console.log("Cluj-Squash-DB running");
+
+// Import mongoose and express
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Connect to the name of the DB = clujsquash
+mongoose.connect('mongodb://localhost/clujsquash');
+let db = mongoose.connection;
+
+// Check connection
+db.once('open', function(){
+    console.log('Connected to mongoDB');
+});
+
+// Check for db error
+db.on('error', function(err){
+    console.log(err);
+});
+
+// Init app
+const app = express();
+
+// Bring in Models...
